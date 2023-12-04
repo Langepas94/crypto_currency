bb8d13057155caf69a2b4d01134dacffdad936d3
Artem <artyom9494@yandex.ru>
2023-12-05 00:14:37 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:crypto_currency/crypto_app.dart';
+import 'package:crypto_currency/firebase_options.dart';
 import 'package:crypto_currency/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  GetIt.I.registerLazySingleton<AbstractCoinsRepository>(() => CryptoCoinsRepository(dio: Dio()));
+  GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
+      () => CryptoCoinsRepository(dio: Dio()));
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   runApp(const CryptoCurrencyApp());
 }
